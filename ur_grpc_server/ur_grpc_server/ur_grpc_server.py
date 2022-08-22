db13c31ece5357ba1a10a2b939f76e7394bccd48
Kim Seong Hyeon <dhkdwk0215@naver.com>
2022-08-22 15:44:14 +0900

    Add files via upload

--- ur_grpc_server/ur_grpc_server/ur_grpc_server.py
+++ ur_grpc_server/ur_grpc_server/ur_grpc_server.py
@@ -1,29 +1,32 @@
 
-"""
-Subscribes to both camera topic (/left_image and /right_image) and creates
-a grpc server from where it can be accessed.
-"""
 from concurrent.futures import ThreadPoolExecutor
+import imp
 from threading import Lock
 import rclpy
 from rclpy.node import Node
+import robot_pose_pb2
+import robot_pose_pb2_grpc
+
 
 import grpc
 
-class Pose(pose_pb2_grpc.PoseService):
+class Pose(robot_pose_pb2_grpc.PoseServicer):
     
     def GetPose(self, request, context):
-        return pose_pb2.PoseReply
+        print(request.id)
+        return robot_pose_pb2.PoseReply
 
 
 
 if __name__ == "__main__":
     print("Start Server...")
-    opt = [('grpc.max_send_message_length', 200000), ('grpc.max_receive_message_length', 200000)]
-    server = grpc.server(ThreadPoolExecutor(max_workers=10), options=opt)
-    camera_pb2_grpc.add_CameraServiceServicer_to_server(CameraSDKServer(), server)
-    pose_pb2_grpc.add_PoseReply_to_server(Pose(), Server)
+
+    server = grpc.server(ThreadPoolExecutor(max_workers=10))
+  
+    robot_pose_pb2_grpc.add_PoseServicer_to_server(Pose(), server)
 
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
+
+
